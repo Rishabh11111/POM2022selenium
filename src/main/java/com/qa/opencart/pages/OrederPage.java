fee799b7a1130d553846ae3c740f00fe57d7aac9
Rishabh11111 <rishabhmishra429@gmail.com>
2022-02-19 00:56:16 -0800

    order page is updated as per Rishabh review comments

--- src/main/java/com/qa/opencart/pages/OrederPage.java
+++ src/main/java/com/qa/opencart/pages/OrederPage.java
@@ -1,8 +1,16 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class OrederPage {
+	
+	
+	By order = By.id("order");
+	
 	public void order() {
-		System.out.println("order");
+		System.out.println("order id : 12345");
 	}
+	
+	
 
 }
